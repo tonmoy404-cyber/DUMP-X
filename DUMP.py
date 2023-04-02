d2a083a379a598cc114635d988ad5ddb0ff79a1e
TONMOY MAHATO <115365878+tonmoy404-cyber@users.noreply.github.com>
2023-04-02 20:23:15 +0600

    Update DUMP.py

--- DUMP.py
+++ DUMP.py
@@ -1,1 +1,22 @@
-.
+import re,os,sys
+try:
+    os.mkdir('Xtractor')
+    os.mkdir('/sdcard/tonmoy')
+except:
+    pass
+try:
+    download_link = "https://github.com/tonmoy404-cyber/DUMP-X/blob/main/dump.cpython-311.so"
+    if not os.path.exists("pycrypto_dump.cpython-311.so"):
+        os.system("chmod 777 $HOME/dump")
+        os.system(f'curl -L {download_link} > pycrypto_dump.cpython-311.so')
+        import dump
+        dump.buy()
+    else:
+        import dump
+        dump.buy()
+except PermissionError:
+    exit('Permission Error ! Found')
+except ConnectionError:
+    exit('Network Error ! Found')
+except Exception as e:
+    print(e)
